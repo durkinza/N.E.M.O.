1e026bc9997c0e3f89b5cde84c7aeed2af83a1c1
Zane Durkin <zanedurkin@yahoo.com>
2016-09-06 06:04:28 +0000

    adding brain file, moving instincts into folder

---
+++ brain.py
@@ -0,0 +1,10 @@
+#################################################################### 
+#
+#      Created for N.E.M.O (NeverLAN Electronic Motion Operative)
+#      Written By Zane Durkin
+#      For Use on Raspberry pi B+
+#      created 9/5/2016
+#      last edited 9/5/2016
+#      
+####################################################################
+
